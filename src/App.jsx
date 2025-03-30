58c75023afb8b4db8cb3cb517758e78e2445e372
Frank2006x <frank2006x@gmail.com>
2025-03-30 23:05:54 +0530

    added page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import Experience from './components/Experience'
 import General from './components/General'
 import Education from './components/Education'
+import Page from './components/page'
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-    <General/>
-    <Education/>
-    <Experience/>
-    </>
+    <div className='main'>
+      <div className='sidebar'>
+        <General/>
+        <Education/>
+        <Experience/>
+      </div>
+      <Page/>
+    </div>
+
+
+
+
+    
   )
 }
 
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,13 +1,16 @@
-function Page(){
-
-
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../css/page.css'
+function Page() {
     return (
-        <div className="cv">
-
-
-            
+        <div className="page">
+            <h2 className="mb-5 mt-3">Frank Xavio</h2>
+            <div className="d-flex flex-direction-column justify-content-between ">
+                <p className="ms-4">Email: frank2006@gmail.com</p>
+                <p className="me-4">Phone no: 3205932590</p>
+            </div>
+            <hr/>
         </div>
-    )
+    );
 }
+
+export default Page;
